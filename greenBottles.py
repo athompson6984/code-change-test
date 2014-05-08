a345c8a281e4b4294d7c8e68bd81a0ad49ee2a57
Abigail Thompson <athompson6984@sohamcollege.org.uk>
2014-05-08 11:55:01 +0100

    Update greenBottles.py
    Completely remade, using a while loop to make it count down to zero, timing, and comments.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,13 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+#10 Green Bottles challenge
+import time #Import time for pauses
+bottles =10 #Create the counting variable
+while bottles > 0: #Start a loop that will print the rhyme and count down to 0
+    print("\n"bottles, " green bottles, sitting on the wall")
+    time.sleep(2)
+    print(bottles, " green bottles, sitting on the wall")
+    time.sleep(2)
+    print("And if 1 green bottle should accidentally fall,")
+    time.sleep(2)
+    bottles = bottles-1 #Count down
+    print("There'll be ", bottles, " green bottles, sitting on the wall.")
+    time.sleep(3)
