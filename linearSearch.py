25eac2cc06481121ab75f9928b1058bad855f3df
Abigail Thompson <athompson6984@sohamcollege.org.uk>
2014-05-15 22:33:57 +0100

    Update linearSearch.py
    Attempted to fix, but couldn't find anything to make it work and am bogged down with homework, so I give up here. Will complete the other task as well to make up for it, as it shouldn't take long.

--- linearSearch.py
+++ linearSearch.py
@@ -1,18 +1,21 @@
 testString = ""
 searchChar = ""
-charList = []
 lastPosition = -1
+replaceChar = ""
 
+while testString == "": #Request a string input to be used through the program
+    testString = input("Please enter some text to search: ")
 
-while testString == "":
-    testString = input("Please enter some text to search : ")
+while len(searchChar) != 1: #Request a character that is to be replaced in the string
+    searchChar = input("Enter a character replace: ")
 
-while len(searchChar) != 1:
-    searchChar = input("Enter a character to search for :")
+while len(replaceChar) != 1: #Request a character to replace the chosen character with
+    replaceChar = input("Enter a character to replace {0} with: ".format(searchChar))
 
-for x in range (len(testString)):
-    if testString[x] == searchChar:
-        charList.append(x-lastPosition)
+for x in range(len(testString)):
+    if testString[x] == searchChar: #Search through the string for the chosen character
+        testString.replace(searchChar,replaceChar) # This isn't working
+        #Should replace the character with the character chosen, but doesn't
         lastPosition = x
 
-print ("I found {0} occurences of {1}".format(len(charList),searchChar))
+print(testString)
