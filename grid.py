d8bfcc831ac17515ad397bfc08e9d25d12688cea
Abigail Thompson <athompson6984@sohamcollege.org.uk>
2014-05-15 22:38:45 +0100

    Update grid.py
    This solution is more efficient in a large-scale program where it could be used more than once,
    But just as a program in itself it's a little bit... unnecessary?

--- grid.py
+++ grid.py
@@ -1,4 +1,6 @@
-print("-------")
-print("| | | |")
-print("-------")
-print("| | | |")
+def printgrid(): #Made it a function
+    for x in range(2): #Used loops to make it better
+        print("---------")
+        print("| | | | |")
+
+printgrid() #conduct the function and print the grid.
